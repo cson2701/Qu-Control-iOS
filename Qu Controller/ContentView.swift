a2b9a2cad9549fb73ba4eea7e1362fdaaf1d2dad
song_cr <gavinsong727@gmail.com>
2026-07-12 18:47:35 +1000

    Initial Commit

---
+++ Qu Controller/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Qu Controller
+//
+//  Created by Chaoran Song on 12/7/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Qu Controller/Qu_ControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Qu_ControllerApp.swift
+//  Qu Controller
+//
+//  Created by Chaoran Song on 12/7/2026.
+//
+
+import SwiftUI
+
+@main
+struct Qu_ControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
